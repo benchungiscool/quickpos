9c4b3cf734faa4f4ae7af17c286dff8101d6c1e8
Ben Chung <benchung61@gmail.com>
2020-11-04 08:18:59 +0000

    Made some progress on the changing menu screen, need to fully implement the stack overflow answer

--- main.py
+++ main.py
@@ -2,26 +2,54 @@ import tkinter as tk
 from pointofsale import PointofSale
 from meta import WindowManagement 
 
+
+## Master class
+class App(tk.Frame):
+    def __init__(self, master=None):
+
+        super().__init__(master)
+
+        self.master = master
+        self.pack()
+
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        
+        self.frames = {}
+
+        for F in (MainMenu, PointofSale):
+
+            page_name = F.__name__
+            frame = F(self.master, self)
+            self.frames[page_name] = frame
+
+            #frame.grid(row=0, column=0, sticky="nsew")
+
+        self.ShowFrame("MainMenu")
+    
+    def ShowFrame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
 ## Main menu class, navigate between elements of the program
 class MainMenu(tk.Frame):
-    def __init__(self, master=None):
+    def __init__(self, master, controller):
         super().__init__(master)
-        self.master = WindowManagement.Clear(self, self.master)
+        self.controller = controller
+        self.master = master
         self.pack()
         self.CreateWidgets()
 
     def CreateWidgets(self):
         self.PointofSale = tk.Button(self)
         self.PointofSale["text"] = "Point of Sale"
-        self.PointofSale["command"] = self.ShowPointofSale
+        self.PointofSale["command"] = lambda:controller.ShowFrame(self,"PointOfSale")
         self.PointofSale.pack(side="left")
-
-    def ShowPointofSale(self):
-        self.master = WindowManagement.Clear(self, self.master)
-        app = PointofSale()
         
 
 if __name__ == "__main__":
     master = tk.Tk()
-    app = MainMenu(master=master)
+    app = App(master=master)
     app.mainloop()
--- pointofsale.py
+++ pointofsale.py
@@ -1,17 +1,13 @@
 import tkinter as tk
-from meta import WindowManagement 
 
 
 class PointofSale(tk.Frame):
-    def __init__(self, master=None):
+    def __init__(self, master, controller):
         super().__init__(master)
         self.master = master
         self.pack()
         self.CreateWidgets()
 
     def CreateWidgets(self):
-        self.Text = tk.Text(self.master)
-        self.Text.pack()
-        self.Text.insert(tk.END, "Benj")
-        
-        
+        self.Text = tk.Label(self.master, text="Benj")
+        self.Text.pack() 
