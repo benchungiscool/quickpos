a455a87909b020c75767a59371626fbdea6eb48f
Ben Chung <benchung61@gmail.com>
2020-11-10 11:15:00 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import font as tkfont
+from pointofsale import PointofSale
 
 class SampleApp(tk.Tk):
 
@@ -16,10 +17,10 @@ class SampleApp(tk.Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
-        self.frames = {}
-        for F in (StartPage, PageOne, PageTwo):
+        self.frames = {"MainMenu", "PointofSale"}
+        for F in (MainMenu, PointofSale):
             page_name = F.__name__
-            frame = F(parent=container, controller=self)
+            frame = F(master=container, controller=self)
             self.frames[page_name] = frame
 
             # put all of the pages in the same location;
@@ -30,49 +31,22 @@ class SampleApp(tk.Tk):
         self.show_frame("StartPage")
 
     def show_frame(self, page_name):
-        '''Show a frame for the given page name'''
+        #Show a frame for the given page name
         frame = self.frames[page_name]
         frame.tkraise()
 
 
-class StartPage(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
+class MainMenu(tk.Frame):
+    def __init__(self, master=None, controller=None):
+        super().__init__(master)
         self.controller = controller
-        label = tk.Label(self, text="This is the start page", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
+        self.master = master
+        self.CreateWidgets()
 
-        button1 = tk.Button(self, text="Go to Page One",
-                            command=lambda: controller.show_frame("PageOne"))
-        button2 = tk.Button(self, text="Go to Page Two",
-                            command=lambda: controller.show_frame("PageTwo"))
-        button1.pack()
-        button2.pack()
-
-
-class PageOne(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="This is page 1", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("StartPage"))
-        button.pack()
-
-
-class PageTwo(tk.Frame):
-
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="This is page 2", font=controller.title_font)
-        label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("StartPage"))
-        button.pack()
+    def CreateWidgets(self):
+        self.Button1 = tk.Button(self, text="Point of Sale", 
+                        command=lambda: self.controller.show_frame("PointofSale"))
+        self.Button1.pack(side="left")
 
 
 if __name__ == "__main__":
