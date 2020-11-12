797210f6cc7de41bb2a80931455a83ae3bca846c
Ben Chung <benchung61@gmail.com>
2020-11-12 08:42:03 +0000

    Got the window manager working

--- main.py
+++ main.py
@@ -1,54 +1,61 @@
 import tkinter as tk
+from tkinter import font as tkfont
 from pointofsale import PointofSale
-from meta import WindowManagement 
 
 
-## Master class
-class App(tk.Frame):
-    def __init__(self, master=None):
+class App(tk.Tk):
 
-        super().__init__(master)
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
 
-        self.master = master
-        self.pack()
+        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
 
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
-        self.frames = {}
+        self.frames = {
+            "MainMenu": MainMenu,
+            "PointofSale": PointofSale,
+        }
 
         for F in (MainMenu, PointofSale):
-
             page_name = F.__name__
             frame = F(master=container, controller=self)
             self.frames[page_name] = frame
 
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
             frame.grid(row=0, column=0, sticky="nsew")
 
         self.ShowFrame("MainMenu")
     
-    def ShowFrame(self, cont):
-        frame = self.frames[cont]
+    def ShowFrame(self, page_name):
+        #Show a frame for the given page name
+        frame = self.frames[page_name]
         frame.tkraise()
 
-## Main menu class, navigate between elements of the program
+
 class MainMenu(tk.Frame):
-    def __init__(self, master, controller):
+    def __init__(self, master=None, controller=None):
         super().__init__(master)
         self.controller = controller
         self.master = master
-        self.pack()
+
         self.CreateWidgets()
 
     def CreateWidgets(self):
         self.Button1 = tk.Button(self, text="Point of Sale", 
                         command=lambda: self.controller.ShowFrame("PointofSale"))
-        self.Button1.pack(side="left")
-        
+        self.Button1.grid(row=5, column=5, pady=105, padx=205)
+
 
 if __name__ == "__main__":
-    master = tk.Tk()
-    app = App(master=master)
+    app = App()
+    app.geometry("500x250")
     app.mainloop()
--- pointofsale.py
+++ pointofsale.py
@@ -5,10 +5,14 @@ class PointofSale(tk.Frame):
     def __init__(self, master, controller):
         super().__init__(master)
         self.master = master
+        self.controller = controller
+
         print("PointofSale Function ran")
+        
         self.CreateWidgets()
         
     def CreateWidgets(self):
-        self.Benj = tk.Label(self.master, text="Benj")
-        self.Benj.pack() 
-
+        self.Button1 = tk.Button(self, text="MainMenu", 
+                        command=lambda: self.controller.ShowFrame("Back"))
+        self.Button1.grid(row=5, column=5)
+       
