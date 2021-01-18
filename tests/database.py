b35c9a0bc570dccd4e4bd867aee39f99bbd5b2e6
Ben Chung <benchung61@gmail.com>
2021-01-18 16:35:08 +0000

    Fixed the RemoveDuplicates function in product.py, now removes duplicates

---
+++ tests/database.py
@@ -0,0 +1,23 @@
+from backend.database import Database
+from backend.product import Product
+
+if __name__ == "__main__": 
+  products = [
+    ["Branston's Beans", 0.75],
+    ["Hovis Bread", 1]
+  ]
+
+  prod = Product()
+  for product in products:
+    for i in range(2):
+      prod.CreateProduct(product[0], product[1])
+
+  instruction = """
+  SELECT * FROM products
+  """
+
+  prod.RemoveDuplicates()
+    
+  for item in prod.db.ReturnRecords(instruction):
+    print(item)
+
